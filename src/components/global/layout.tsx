9acf0c5875e8ea65ffbb04b4d0f663235f56812d
Tobi Idowu <idtobi8@gmail.com>
2024-05-06 10:15:24 +0100

    Updated layout & login

--- src/components/global/layout.tsx
+++ src/components/global/layout.tsx
@@ -13,7 +13,7 @@ const Layout = ({ children, className, hasBackground }: LayoutProps) => {
         backgroundRepeat: "repeat",
         backgroundSize: "contain",
       }}
-      className={`${className} w-full relative min-h-[100vh] md:pb-[200px]`}
+      className={`${className} w-full relative flex items-start justify-between flex-col min-h-[100vh] md:pb-[200px]`}
     >
       <Container className="py-3">
         <img src={logo} alt="Logo" className="w-10 h-10" />
